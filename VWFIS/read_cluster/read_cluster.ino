49087cabd5380d1f684333a600834a41c72e3e9a
Tomas Kovacik <tomas.kovacik@yahoo.com>
2014-11-04 12:08:20 +0100

    remove unneeded files

--- VWFIS/read_cluster/read_cluster.ino
+++ VWFIS/read_cluster/read_cluster.ino
@@ -1,92 +1,180 @@
-//#define intENA 2
-//#define intCLK 3
-//#define intDATA 4
-//#define ENA_in 21
-//#define CLK 20
-//#define DATA 19
-//#define ENA_out 18
-#define intENA 0
-#define intCLK 1
-//#define intDATA 4
-#define ENA_in 2
-#define CLK 3
-#define DATA 11
-//#define ENA_out 18
+//enable need to have pullup to 5V
+
+#define FIS_READ_intCLK 1 //interupt on FIS_READ_CLK line
+#define FIS_READ_CLK 3 //clk
+#define FIS_READ_DATA 12 //data
+#define FIS_READ_ENA 2 //enable
+#define FIS_READ_intENA 0 //interupt on FIS_READ_ENA line
+
 #include <LiquidCrystal.h>
-volatile int _WORD[144];
-int text[16];
-int i_char;
-int i_sum_of_received_chars;
-volatile int _bit=0;
-int capturing=0;
-long delta_last_byte=0;
-volatile int ack=0;
+
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-void read_data_line(){
-if(_bit>143){
-_bit=0;
+volatile uint8_t FIS_READ_adr=0;
+volatile uint64_t FIS_READ_msg1=0;
+volatile uint64_t FIS_READ_msg2=0;
+volatile uint8_t FIS_READ_cksum=0;
+volatile uint8_t FIS_READ_msgbit=0;
+volatile uint8_t FIS_READ_newmsg1=0;
+volatile uint8_t FIS_READ_newmsg2=0;
+volatile uint8_t FIS_READ_adrok=0;
+volatile uint8_t FIS_READ_cksumok=0;
+volatile uint8_t FIS_READ_tmp_cksum=0;
+volatile uint8_t packet_counter=0;
+
+void FIS_READ_read_data_line(){
+  if (!digitalRead(FIS_READ_CLK)){
+    if(!FIS_READ_adrok){
+      FIS_READ_read_adr();
+    }
+    else if (!FIS_READ_newmsg1){
+      FIS_READ_read_msg1();
+    }
+    else if (!FIS_READ_newmsg2){
+      FIS_READ_read_msg2();
+    }
+    else if (!FIS_READ_cksumok){
+      FIS_READ_read_cksum();
+    }
+  }
 }
- if (!digitalRead(CLK)){// && digitalRead(ENA_in)){
-    _WORD[_bit++]=digitalRead(DATA);
- }
+
+void FIS_READ_read_cksum(){
+  if(digitalRead(FIS_READ_DATA)){
+    FIS_READ_cksum = (FIS_READ_cksum<<1) | 0x00000001;
+    FIS_READ_msgbit++;
+  }
+  else
+  {
+    FIS_READ_cksum = (FIS_READ_cksum<<1);
+    FIS_READ_msgbit++;
+  }
+  if (FIS_READ_msgbit==8)
+  {
+    FIS_READ_tmp_cksum=(0xFF^FIS_READ_adr);
+    for (int i=56;i>=0;i=i-8){
+      FIS_READ_tmp_cksum=FIS_READ_tmp_cksum+(0xFF^((FIS_READ_msg1>>i) & 0xFF))
+        +(0xFF^((FIS_READ_msg2>>i) & 0xFF));
+    }
+    if((FIS_READ_tmp_cksum%256)==FIS_READ_cksum)
+    FIS_READ_cksumok=1;
+    packet_counter++;
+    FIS_READ_msgbit=0;
+  }
+
 }
 
-void enaUP(){
-  //starting of WORD
-  _bit=0;
+void FIS_READ_read_msg1(){
+  if(digitalRead(FIS_READ_DATA)){
+    FIS_READ_msg1 = (FIS_READ_msg1<<1) | 0x00000001;
+    FIS_READ_msgbit++;
+  }
+  else
+  {
+    FIS_READ_msg1 = (FIS_READ_msg1<<1);
+    FIS_READ_msgbit++;
+  }
+  if (FIS_READ_msgbit==64)
+  {
+    FIS_READ_newmsg1=1;
+    FIS_READ_msgbit=0;
+  }
 }
 
+void FIS_READ_read_msg2(){
+  if(digitalRead(FIS_READ_DATA)){
+    FIS_READ_msg2 = (FIS_READ_msg2<<1) | 0x00000001;
+    FIS_READ_msgbit++;
+  }
+  else
+  {
+    FIS_READ_msg2 = (FIS_READ_msg2<<1);
+    FIS_READ_msgbit++;
+  }
+  if (FIS_READ_msgbit==64)
+  {
+    FIS_READ_newmsg2=1;
+    FIS_READ_msgbit=0;
+  }
+}
 
+void FIS_READ_read_adr(){
+  if(digitalRead(FIS_READ_DATA)){
+    FIS_READ_adr = (FIS_READ_adr<<1) | 0x00000001;
+    FIS_READ_msgbit++;
+  }
+  else
+  {
+    FIS_READ_adr = (FIS_READ_adr<<1);
+    FIS_READ_msgbit++;
+  }
+  if (FIS_READ_msgbit==8)// && FIS_READ_adr==0xF)
+  {
+    FIS_READ_adrok=1;
+    FIS_READ_msgbit=0;
+  }
+}
+void FIS_READ_detect_ena_line(){
+  if(digitalRead(FIS_READ_ENA)) {
+    attachInterrupt(FIS_READ_intCLK,FIS_READ_read_data_line,CHANGE);
+    detachInterrupt(FIS_READ_intENA);
+  } else {
+    detachInterrupt(FIS_READ_intCLK);
+  }
+}
 
-//void f_ack(){
-//    detachInterrupt(intENA);
-//    digitalWrite(ENA_out,LOW);
-//    delay(500);
-//    digitalWrite(ENA_out,HIGH);
-//    delay(10);
-//    attachInterrupt(intENA,enaUP,RISING);
-//}
-
-void setup() {
- lcd.begin(16,2);
- lcd.home();
- lcd.clear();
-  attachInterrupt(intENA,enaUP,RISING);
-  attachInterrupt(intCLK,read_data_line,CHANGE);
-  pinMode(DATA,INPUT);
-pinMode(CLK,INPUT);
-  pinMode(ENA_in,INPUT);
- // pinMode(ENA_out,OUTPUT);
- // digitalWrite(ENA_out,HIGH);
+void setup() { 
+  lcd.begin(16,2);
+  lcd.home();
+  lcd.print("start");
   delay(500);
- // f_ack();
+  lcd.clear();
+  digitalWrite(FIS_READ_ENA,HIGH);
+  delayMicroseconds(100);
+  digitalWrite(FIS_READ_ENA,LOW);
+  delayMicroseconds(100);
+  digitalWrite(FIS_READ_ENA,HIGH);
+  delayMicroseconds(37);
+  digitalWrite(FIS_READ_ENA,LOW);
+  delayMicroseconds(37);
+  pinMode(FIS_READ_ENA,INPUT);//_PULLUP);
+  pinMode(FIS_READ_CLK,INPUT_PULLUP);
+  pinMode(FIS_READ_DATA,INPUT_PULLUP);
+  attachInterrupt(FIS_READ_intENA,FIS_READ_detect_ena_line,CHANGE);
+ //attachInterrupt(FIS_READ_intCLK,FIS_READ_read_data_line,CHANGE);
 }
 
 void loop() {
- 
-if(_bit==144){ //last bit is 143
-
-if ((_WORD[0]*128+_WORD[1]*64+_WORD[2]*32+_WORD[3]*16+_WORD[4]*8+_WORD[5]*4+_WORD[6]*2+_WORD[7])==15) {
-int line_int=0;
-  i_sum_of_received_chars=240;
-  for (int i=8;i<136;i++){
-    i_char=(255-(_WORD[i]*128+_WORD[++i]*64+_WORD[++i]*32+_WORD[++i]*16+_WORD[++i]*8+_WORD[++i]*4+_WORD[++i]*2+_WORD[++i]));
-    text[line_int++]=i_char;
-    i_sum_of_received_chars+=i_char;
-  }
- if((i_sum_of_received_chars%256)==(_WORD[136]*128+_WORD[137]*64+_WORD[138]*32+_WORD[139]*16+_WORD[140]*8+_WORD[141]*4+_WORD[142]*2+_WORD[143])){
-lcd.home();
-lcd.clear();
-  for(int i=0;i<16;i++){
-    lcd.write(text[i]);
-    if (i==7)
-      lcd.setCursor(0,1);
+  if(FIS_READ_cksumok){     
+    lcd.home();
+    lcd.clear();
+    lcd.print(FIS_READ_adr,DEC);
+    lcd.setCursor (3,0);
+    for(int i=56;i>=0;i=i-8){
+      lcd.write(0xFF^((FIS_READ_msg1>>i) & 0xFF));
     }
- }
+    lcd.setCursor(0,1);
+    for(int i=56;i>=0;i=i-8){
+      lcd.write(0xFF^((FIS_READ_msg2>>i) & 0xFF));   
+    }
+    FIS_READ_newmsg1=0;
+    FIS_READ_newmsg2=0;
+    FIS_READ_adrok=0;
+    FIS_READ_cksumok=0;
+    pinMode(FIS_READ_ENA,OUTPUT);
+    digitalWrite(FIS_READ_ENA,HIGH);
+    delayMicroseconds(100);
+    digitalWrite(FIS_READ_ENA,LOW);
+    delayMicroseconds(100);
+    pinMode(FIS_READ_ENA,INPUT);//_PULLUP);
+    attachInterrupt(FIS_READ_intENA,FIS_READ_detect_ena_line,CHANGE);
+    lcd.setCursor(6,1);
+    lcd.print(millis());
+  }
+  lcd.setCursor(12,1);
+  lcd.print(packet_counter);  
+  delay(1000);
 }
- //f_ack();
-}
-}
+
 
 
